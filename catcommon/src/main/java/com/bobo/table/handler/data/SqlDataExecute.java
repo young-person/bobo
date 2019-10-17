b71ef51179c374cd874e98c61d3ae3e3076df020
zbcheng <zbcheng@iflytek.com>
2019-10-17 09:48:18 +0800

    新增资源目录
    Signed-off-by: zbcheng <zbcheng@iflytek.com>

--- catcommon/src/main/java/com/bobo/table/handler/data/SqlDataExecute.java
+++ catcommon/src/main/java/com/bobo/table/handler/data/SqlDataExecute.java
@@ -89,11 +89,11 @@ public class SqlDataExecute extends BaseClass implements DataExecute<Condition> 
             if (CEnum.REAL.name().equals(key)){
                 for (Baseid b : list) {
                     queryParse = new ParseRealResult();
-                    sql = parseCondition.getWhereConditionRealSql(b);
+                    sql = parseCondition.getWhereConditionRealSql(b).toString();
                 }
             }else{
                 queryParse = new ParseSumResult();
-                sql = parseCondition.getWhereConditionSql();
+//                sql = parseCondition.getWhereConditionSql().toString();
             }
             SimpleResult result = queryParse.parse(key, sql, hd, list);
             all.add(result);
