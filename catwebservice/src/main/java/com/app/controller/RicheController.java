e25ff631cafd9eb78a2ae8a907749851c7849261
windlybobo <czb199345>
2019-12-09 23:53:32 +0800

    新增资源目录
    Signed-off-by: windlybobo <czb199345>

--- catwebservice/src/main/java/com/app/controller/RicheController.java
+++ catwebservice/src/main/java/com/app/controller/RicheController.java
@@ -12,6 +12,9 @@ import com.app.crawler.riches.BRiches;
 import com.app.crawler.riches.pojo.Bean;
 import com.app.crawler.riches.pojo.Data;
 import com.app.crawler.riches.pojo.ExcelUser;
+import com.app.crawler.riches.pojo.HistoryBean;
+import com.app.crawler.riches.producer.Persist;
+import com.app.crawler.riches.producer.PersistTxt;
 import com.app.crawler.riches.producer.RLoadXml;
 import com.app.service.impl.ReceiveRichesImpl;
 import com.bobo.base.BaseClass;
@@ -94,6 +97,102 @@ public class RicheController extends BaseClass{
 		return view;
 	}
 
+	/**
+	 * 初始化数据
+	 */
+	@GetMapping(value = "initStock")
+	@ResponseBody
+	public void initList(){
+		int d = 50;//关于天的间隔
+		int w = 380;//关于周的间隔
+
+		File file = null;
+		try {
+			file = ResourceUtils.getFile(catXml.getDataPath());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		List<String> filter = new ArrayList<>();
+
+		Persist persist= new PersistTxt();
+		RLoadXml loadXml = new RLoadXml();
+
+		Data data = loadXml.getDataFromXml(catXml.getDataPath());
+		if (Objects.nonNull(data.getBeans())){
+			for(int index = 0; index < data.getBeans().size(); index ++){
+				Bean bean1 = data.getBeans().get(index);
+				filter.add(bean1.getCode());
+			}
+		}
+		Map<String, String> typeName = new HashMap<String, String>() {
+			private static final long serialVersionUID = 6893069965305064089L;
+
+			{
+				put("4621", "创业");
+				put("4614", "深证");
+				put("4353", "沪A");
+				put("4609", "深A");
+			}
+		};
+		String s1 = null, s2 = null;
+		for (File f : file.listFiles()) {
+			if (!f.isDirectory() || !typeName.containsValue(f.getName())) {
+				continue;
+			}
+			for (File e : f.listFiles()) {
+				try{
+					List<HistoryBean> datas = persist.readHistoryFromFile(e);
+
+					Collections.sort(datas, (o1, o2) -> o2.getDate().compareTo(o1.getDate()));
+					List<HistoryBean> list1 = null;
+					List<HistoryBean> list2 = null;
+					if(datas.size() <= d){
+						list1 = datas.subList(0,datas.size());
+					}else if((datas.size() > d && datas.size() <= w)){
+						list1 = datas.subList(0,d);
+						list2 = datas.subList(0,datas.size());
+					}else if(datas.size() > w){
+						list1 = datas.subList(0,d);
+						list2 = datas.subList(0,w);
+					}
+					Collections.sort(list1, Comparator.comparing(HistoryBean::getClosePrice));
+					s1 = list1.get(0).getClosePrice().replace(",","");
+
+					if (Objects.isNull(list2)){
+						s2 = String.valueOf(Float.valueOf(s1.replace(",","")) * 1.05);
+					}else{
+						Collections.sort(list2, Comparator.comparing(HistoryBean::getClosePrice));
+						s2 = list2.get(0).getClosePrice();
+					}
+
+					StringBuilder builder = new StringBuilder();
+					if (Float.valueOf(s1.replace(",","")) >= Float.valueOf(s2.replace(",",""))){
+						builder.append(s2).append("-").append(s1);
+					}else{
+						builder.append(s1).append("-").append(s2);
+					}
+					String name = e.getName();
+					String[] arr = name.replace(".txt", "").split("_");
+					if (filter.contains(arr[0]) || "创业".equals(f.getName())){
+						continue;
+					}
+					RichesData bean = getChooice(arr[0]);
+					List<Property> list = loadXml.converClassToModel(bean);
+					Bean b = new Bean();
+					b.setName(bean.getF14());
+					b.setMark(builder.toString());//监听值
+					b.setProperties(list);
+					b.setCode(bean.getF12());
+					b.setMacd("0");
+					data.getBeans().add(b);
+
+				} catch (IllegalAccessException | IOException | InvocationTargetException | NoSuchMethodException ex) {
+					LOGGER.error("进行初始化监控数据失败,文件:{}",f.getName(),e);
+				}
+			}
+		}
+		loadXml.convertToXml(data,catXml.getDataPath());
+	}
 	/**
 	 * 添加一个需要监听得数据指标  根据代码或者名称进行添加
 	 * @param code
